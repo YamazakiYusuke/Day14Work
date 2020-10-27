81a178372e9f842e0bda155565e8fabdc20cdd67
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-27 21:24:06 +0900

    Routes.rb ルートをログイン画面に設定

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,6 @@
 class Blog < ApplicationRecord
+  validates :title,   presence: true,
+                        length: { maximum: 20 }
+  validates :content, presence: true,
+                        length: { maximum: 140 }
 end
